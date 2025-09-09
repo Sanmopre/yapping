dfc745303c91f87da97e237f89412aa6013b5048
Santiago Moliner <moliner.santiago@gmail.com>
2025-09-09 22:07:36 +0200

    Dummy input and chat box for client

--- packages/client/src/main.cpp
+++ packages/client/src/main.cpp
@@ -1,32 +1,166 @@
-// Dear ImGui: standalone example application for SDL2 + SDL_Renderer
-// (SDL is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
+#include "tcp_client.h"
+#include "cmake_constants.h"
 
-// Learn about Dear ImGui:
-// - FAQ                  https://dearimgui.com/faq
-// - Getting Started      https://dearimgui.com/getting-started
-// - Documentation        https://dearimgui.com/docs (same as your local docs/ folder).
-// - Introduction, links and more at the top of imgui.cpp
-
-// Important to understand: SDL_Renderer is an _optional_ component of SDL2.
-// For a multi-platform app consider using e.g. SDL+DirectX on Windows and SDL+OpenGL on Linux/OSX.
-
+// imgui
 #include "imgui.h"
 #include "imgui_impl_sdl2.h"
 #include "imgui_impl_sdlrenderer2.h"
-#include <stdio.h>
-#include <SDL.h>
+#include "imgui_internal.h"
 
-#if !SDL_VERSION_ATLEAST(2,0,17)
-#error This backend requires SDL 2.0.17+ because of SDL_RenderGeometry() function
-#endif
+// std
+#include <stdio.h>
+
+// sdl
+#include "SDL.h"
+
+// cli11
+#include "CLI/CLI.hpp"
+
+
+static void MessageBubble(const char* text,
+                          bool is_outgoing,
+                          float avail_width,
+                          ImU32 col_bg_incoming  = IM_COL32(235, 236, 240, 255), // light gray
+                          ImU32 col_bg_outgoing  = IM_COL32(180, 232, 149, 255), // green
+                          ImU32 col_text         = IM_COL32(0, 0, 0, 255))
+{
+    ImDrawList* dl = ImGui::GetWindowDrawList();
+
+    const float padding_x = 10.0f;
+    const float padding_y = 8.0f;
+    const float rounding  = 12.0f;
+    const float space_y   = 6.0f;
+
+    // Limit bubble width to ~65% of available space
+    float max_width = ImClamp(avail_width * 0.65f, 80.0f, avail_width);
+
+    // Measure text with wrapping
+    ImVec2 text_size = ImGui::CalcTextSize(text, nullptr, true, max_width);
+
+    // Bubble size = text + padding
+    ImVec2 bubble_size(text_size.x + padding_x * 2.0f,
+                       text_size.y + padding_y * 2.0f);
+
+    // Cursor position in screen space
+    ImVec2 cursor_screen = ImGui::GetCursorScreenPos();
+
+    // Horizontal placement
+    ImVec2 bubble_min;
+    if (is_outgoing) {
+        bubble_min.x = cursor_screen.x + avail_width - bubble_size.x;
+    } else {
+        bubble_min.x = cursor_screen.x;
+    }
+    bubble_min.y = cursor_screen.y;
+
+    ImVec2 bubble_max(bubble_min.x + bubble_size.x,
+                      bubble_min.y + bubble_size.y);
+
+    // Draw bubble background
+    ImU32 col_bg = is_outgoing ? col_bg_outgoing : col_bg_incoming;
+    dl->AddRectFilled(bubble_min, bubble_max, col_bg, rounding);
+
+    // Draw text
+    ImVec2 text_pos(bubble_min.x + padding_x, bubble_min.y + padding_y);
+    ImGui::SetCursorScreenPos(text_pos);
+
+    float inner_wrap = text_pos.x + (bubble_size.x - padding_x * 2.0f);
+    ImGui::PushTextWrapPos(inner_wrap);
+    ImGui::PushStyleColor(ImGuiCol_Text, col_text);
+    ImGui::TextUnformatted(text);
+    ImGui::PopStyleColor();
+    ImGui::PopTextWrapPos();
+
+    // Advance cursor for next bubble
+    ImGui::SetCursorScreenPos(ImVec2(cursor_screen.x, bubble_max.y + space_y));
+}
+
 
 // Main code
-int main(int, char**)
+int main(int argc, char** argv)
 {
+    CLI::App clientApplication(CLIENT_DESCRIPTION);
+    clientApplication.set_version_flag("--version", PROJECT_VERSION);
+
+    // Arguments for the client
+    std::string username = "random";
+    std::string serverIp;
+    std::string loggingFolder = "./logs";
+    u16 serverPort;
+
+    clientApplication.add_option("-u,--username", username,
+        "Username for the client to use when connecting");
+
+    clientApplication.add_option("-i,--ip", serverIp,
+        "IPv4 address of the server to connect to")
+        ->required()
+        ->check(CLI::ValidIPV4);
+
+    clientApplication.add_option("-p,--port", serverPort,
+        "Port of the server to connect to")
+        ->required()
+        ->check(CLI::Range(1, 65535));
+
+    clientApplication.add_option("-l,--log-folder", loggingFolder, "Path tp the folder where the logs from the application will be generated.")
+   ->check(CLI::ExistingDirectory);
+
+
+    CLI11_PARSE(clientApplication, argc, argv);
+
+    const auto timeStampStringForFile = makeSafeForFilename(getTimeStamp(currentSecondsSinceEpoch()));
+    const std::string logFile = std::string(CLIENT_TARGET_NAME) + "_" + timeStampStringForFile + ".log";
+
+    const auto logger = getLogger(
+        CLIENT_TARGET_NAME,
+        (std::filesystem::path(loggingFolder) / logFile).string()
+    );
+
+  logger->info("Starting {} version {}", CLIENT_TARGET_NAME, PROJECT_VERSION);
+
+  SimpleTcpClient cli;
+    std::vector<server::messages::NewMessageReceived> messages;
+
+  cli.on_connect([&cli, &logger, &username]
+  {
+      client::messages::InitialConnection msg;
+      msg.username = username;
+      cli.write(msg);
+      logger->info("Connected to server");
+  });
+
+  cli.on_disconnect([&cli, &logger]
+  {
+      client::messages::Disconnect msg;
+      msg.reason = "Just left";
+      cli.write(msg);
+      logger->info("Disconnected from server");
+  });
+
+  cli.on_message([&cli , &logger, &messages](const server::messages::ServerMessage&& msg)
+  {
+      std::visit(overloaded{
+  [](const server::messages::UserConnected& v)
+  {
+      std::cout << "Connected = " << v.username << "\n";
+  },
+  [](const server::messages::UserDisconnected& v) {
+      std::cout << "User disconnected: = " << v.reason << "\n";
+  },
+  [&messages](const server::messages::NewMessageReceived& v)
+  {
+      messages.emplace_back(v);
+      std::cout << "Message = " << v.username << "\n";
+  }
+      }, msg);
+  });
+
+  cli.connect(serverIp, serverPort);
+
+
     // Setup SDL
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0)
     {
-        printf("Error: %s\n", SDL_GetError());
+        logger->error("Failed to initialize SDL - {}", SDL_GetError());
         return -1;
     }
 
@@ -36,22 +170,24 @@ int main(int, char**)
 #endif
 
     // Create window with SDL_Renderer graphics context
-    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+    constexpr auto window_flags = static_cast<SDL_WindowFlags>(SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
     SDL_Window* window = SDL_CreateWindow("Dear ImGui SDL2+SDL_Renderer example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
     if (window == nullptr)
     {
-        printf("Error: SDL_CreateWindow(): %s\n", SDL_GetError());
+        logger->error("Failed to create window - {}", SDL_GetError());
         return -1;
     }
+
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
-    if (renderer == nullptr)
+    if (!renderer)
     {
-        SDL_Log("Error creating SDL_Renderer!");
+        logger->error("Failed to create renderer");
         return -1;
     }
-    //SDL_RendererInfo info;
-    //SDL_GetRendererInfo(renderer, &info);
-    //SDL_Log("Current SDL_Renderer: %s", info.name);
+
+    SDL_RendererInfo info;
+    SDL_GetRendererInfo(renderer, &info);
+    logger->info("SDL_RendererInfo info: {}", info.name);
 
     // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
@@ -60,44 +196,19 @@ int main(int, char**)
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
-    // Setup Dear ImGui style
-    ImGui::StyleColorsDark();
-    //ImGui::StyleColorsLight();
-
     // Setup Platform/Renderer backends
     ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
     ImGui_ImplSDLRenderer2_Init(renderer);
 
-    // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-    // - If the file cannot be loaded, the function will return a nullptr. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-    // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use Freetype for higher quality font rendering.
-    // - Read 'docs/FONTS.md' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-    //io.Fonts->AddFontDefault();
-    //io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf", 18.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, nullptr, io.Fonts->GetGlyphRangesJapanese());
-    //IM_ASSERT(font != nullptr);
-
     // Our state
     bool show_demo_window = true;
     bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    constexpr auto clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
     // Main loop
     bool done = false;
     while (!done)
     {
-        // Poll and handle events (inputs, window resize, etc.)
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
-        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
         SDL_Event event;
         while (SDL_PollEvent(&event))
         {
@@ -118,42 +229,40 @@ int main(int, char**)
         ImGui_ImplSDL2_NewFrame();
         ImGui::NewFrame();
 
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
-
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+        if (ImGui::Begin("Input"))
         {
-            static float f = 0.0f;
-            static int counter = 0;
+            static char buf[256] = "";
 
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
+            // Or if you want it to send on Enter:
+            if (ImGui::InputText("##msg", buf, IM_ARRAYSIZE(buf), ImGuiInputTextFlags_EnterReturnsTrue))
+            {
+                client::messages::NewMessage msg;
+                msg.message = buf;
+                cli.write(msg);
+                buf[0] = '\0';
+            }
             ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+            if ( ImGui::Button("Send"))
+            {
+                client::messages::NewMessage msg;
+                msg.message = buf;
+                cli.write(msg);
+                buf[0] = '\0';
+            }
             ImGui::End();
         }
 
-        // 3. Show another simple window.
-        if (show_another_window)
+        if (ImGui::Begin("Chat"))
         {
-            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
+            float avail_width = ImGui::GetContentRegionAvail().x;
+            for (const auto& message : messages)
+            {
+                MessageBubble(message.message.c_str(), false, avail_width);
+            }
             ImGui::End();
         }
+
+
 
         // Rendering
         ImGui::Render();
@@ -164,6 +273,7 @@ int main(int, char**)
         SDL_RenderPresent(renderer);
     }
 
+
     // Cleanup
     ImGui_ImplSDLRenderer2_Shutdown();
     ImGui_ImplSDL2_Shutdown();
@@ -172,6 +282,10 @@ int main(int, char**)
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+
+    // stop server
+    cli.stop();
+
 
     return 0;
 }
